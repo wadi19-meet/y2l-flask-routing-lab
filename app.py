0a6dd74086f43d2e0092de776a617984100a489f
neomy <neom21leva@gmail.com>
2018-10-25 18:51:26 +0300

    flask

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+	return render_template("shop.html")
+	# return 'hi'
 
 if __name__ == '__main__':
    app.run(debug = True)
